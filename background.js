a041351a3fa32d6bd60c0c7fcc78e0ea5705f6d2
Ondrej Sevcik <hi@ondrejsevcik.com>
2024-05-18 20:41:06 +0200

    Make it work with ChatGPT.com

--- background.js
+++ background.js
@@ -1,39 +1,21 @@
-browser.runtime.onMessage.addListener((data, sender) => {
-  console.log({ data, sender });
-
-  const url = "https://api.openai.com/v1/chat/completions";
-  const OPENAI_API_KEY = "TODO";
-
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${OPENAI_API_KEY}`,
-  };
-
-  const body = JSON.stringify({
-    model: "gpt-4",
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful assistant.",
-      },
-      {
-        role: "user",
-        content: `Summarize this text using bullet points: 
-
-        ${data.text}`,
-      },
-    ],
-  });
-
-  fetch(url, { method: "POST", headers, body })
-    .then((response) => response.json())
-    .then((data) => console.log(data?.choices[0]?.message?.content))
-    .catch((error) => console.error("Error:", error));
-});
-
-// When the browser_action is clicked, inject a content script into the current page
-browser.browserAction.onClicked.addListener((tab) => {
-  browser.tabs.executeScript(tab.id, {
-    file: "contentScript.js",
+const CONTEXT_MENU_ID = "summarize-context-menu";
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("installed context menu");
+  chrome.contextMenus.create({
+    id: CONTEXT_MENU_ID,
+    title: "Summarize in ChatGPT",
+    contexts: ["selection"],
   });
 });
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  saveText(info.selectionText);
+
+  chrome.tabs.create({ url: "https://chatgpt.com" });
+});
+
+function saveText(text) {
+  chrome.storage.local.set({ text: text }, function () {
+    console.debug("Text is set to " + text);
+  });
+}
--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,60 @@
-// Get all text from the body element
-var bodyText = document.body.innerText;
+chrome.storage.local.get("text", function (data) {
+  const text = data.text || "";
+  chrome.storage.local.remove("text");
 
-// Send it back to the background script
-browser.runtime.sendMessage({ text: bodyText });
+  if (!text.trim()) {
+    console.debug("No text found in storage");
+    return;
+  }
+
+  const prompt = `Give me key ideas from this text: ${text}`;
+
+  querySelectorPromise("textarea")
+    .then(async (textarea) => {
+      changeValue(textarea, prompt);
+
+      const form = await querySelectorPromise("form");
+      let submitButton = form.querySelectorAll("button")[1];
+      submitButton?.click();
+    })
+    .catch(console.error);
+});
+
+function querySelectorPromise(selector, timeout = 500) {
+  return new Promise((resolve, reject) => {
+    let intervalId = null;
+    let timeoutId = null;
+
+    const checkElement = () => {
+      const element = document.querySelector(selector);
+      if (element) {
+        clearInterval(intervalId);
+        clearTimeout(timeoutId);
+        resolve(element);
+        console.debug(`Element ${selector} found`);
+      }
+    };
+
+    intervalId = setInterval(checkElement, timeout);
+    timeoutId = setTimeout(() => {
+      clearInterval(intervalId);
+      reject(
+        new Error(
+          `Element with selector "${selector}" was not found within the timeout period.`
+        )
+      );
+    }, 5000); // Maximum wait for 5 seconds.
+  });
+}
+
+function changeValue(textarea, value) {
+  textarea.value = value;
+
+  let event = new Event("input", {
+    bubbles: true,
+    cancelable: true,
+  });
+
+  textarea.dispatchEvent(event);
+  console.debug("Changed value of textarea");
+}
