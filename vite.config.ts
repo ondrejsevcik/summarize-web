5d0c7d313f29ef8d3744bb13c086463fde1f7e18
Ondrej Sevcik <hi@ondrejsevcik.com>
2024-07-02 20:50:36 +0200

    Adjust vite build

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import { defineConfig } from "vite";
+import browserExtension from "vite-plugin-web-extension";
+
+function root(...paths: string[]): string {
+  return path.resolve(__dirname, ...paths);
+}
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: root("dist"),
+    emptyOutDir: true,
+  },
+  plugins: [
+    browserExtension({
+      manifest: "manifest.json",
+      // additionalInputs: ["popup.html"],
+      watchFilePaths: [root("src/manifest.json")],
+      browser: process.env.TARGET || "chrome",
+    }),
+  ],
+});
