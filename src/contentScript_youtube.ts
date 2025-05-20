bb38d59a90024ee07c58cb9c54ef50cf9f1cfbc7
Ondrej Sevcik <4136929+ondrejsevcik@users.noreply.github.com>
2025-05-20 23:55:29 +0200

    feat: refactor YouTube content retrieval to use async query selector and improve error handling

--- src/contentScript_youtube.ts
+++ src/contentScript_youtube.ts
@@ -1,8 +1,6 @@
-import { Readability } from "@mozilla/readability";
-import { querySelectorPromise } from "./utils";
+import { querySelectorAsync } from "./utils";
 import { GET_YOUTUBE_CONTENT, type Youtube } from "./types";
 
-// TODO add youtube support
 // Cross-browser compatible approach
 // @ts-ignore
 const browserAPI = typeof browser !== "undefined" ? browser : chrome;
@@ -10,33 +8,25 @@ const browserAPI = typeof browser !== "undefined" ? browser : chrome;
 browserAPI.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	console.debug("Request Action:", request.action);
 	if (request.action === GET_YOUTUBE_CONTENT) {
-		// TODO fix me
-		// TODO youtube does not work now!
 		getYoutubeContent().then((response) => sendResponse(response));
 		return true; // Keep the message channel open for asynchronous response
 	}
 });
 
 async function getYoutubeContent(): Promise<Youtube> {
-	const showTranscriptBtn = document.querySelector<HTMLButtonElement>(
+	const showTranscriptBtn = await querySelectorAsync<HTMLButtonElement>(
 		'button[aria-label="Show transcript"]',
 	);
-	console.debug("Transcript button:", showTranscriptBtn);
-	// if (!showTranscriptBtn) {
-	//   alert("Transcript button not found. Please check if the transcript is available for this video.");
-	//   throw new Error("Transcript button not found");
-	// }
 
-	console.log("clicking btn", showTranscriptBtn);
-	showTranscriptBtn?.click();
+	showTranscriptBtn.click();
 
-	await querySelectorPromise(".segment-text");
+	await querySelectorAsync(".segment-text");
+	const transcriptNode = await querySelectorAsync<HTMLElement>(
+		"ytd-transcript-segment-list-renderer",
+	);
 
 	return {
 		title: document.title.replace(" - YouTube", ""),
-		transcript:
-			document.querySelector<HTMLElement>(
-				"ytd-transcript-segment-list-renderer",
-			)?.innerText ?? "",
+		transcript: transcriptNode.innerText,
 	};
 }
