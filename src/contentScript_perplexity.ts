dbacd90d28406d62a74e7c9f41a9ea14392522cb
Ondrej Sevcik <4136929+ondrejsevcik@users.noreply.github.com>
2025-06-04 08:44:41 +0200

    chore: adjust disable web search

--- src/contentScript_perplexity.ts
+++ src/contentScript_perplexity.ts
@@ -79,6 +79,7 @@ Tweet content
 ${tweet.tweetContent}`;
 
 	await changePerplexityTextareaValue(prompt);
+	await disableWebSearch();
 
 	await submitPrompt();
 }
@@ -87,13 +88,10 @@ async function runPageSummarization(pageData: Page) {
 	const prompt = "Give me key ideas from the attached file.";
 	await changePerplexityTextareaValue(prompt);
 
-	await disableWebSearch();
-
 	const fileContent = `Title: ${pageData.title}\n\nContent:\n${pageData.textContent}`;
 	await uploadFile(fileContent);
 
 	await disableWebSearch();
-
 	await submitPrompt();
 }
 
@@ -107,12 +105,13 @@ async function runSummarizeSelection(selectedText: string) {
 
 async function runYoutubeSummarization(youtubeData: Youtube) {
 	const prompt = "Give me key ideas from the attached YouTube transcript.";
+	// const prompt = "Analyze this video transcript and provide a comprehensive summary that includes: 1) Main topics discussed, 2) Key points and insights, 3) Important quotes or specific mentions, 4) Any actionable items or conclusions. Structure your response with clear headings and bullet points where appropriate."
 	await changePerplexityTextareaValue(prompt);
-
-	await disableWebSearch();
 
 	const fileContent = `Title: ${youtubeData.title}\n\nContent:\n${youtubeData.transcript}`;
 	await uploadFile(fileContent);
+
+	await disableWebSearch();
 	await submitPrompt();
 }
 
@@ -129,15 +128,19 @@ async function disableWebSearch() {
 	assertNonNullish(setSourcesButton, "setSourcesButton is null");
 	setSourcesButton.click();
 
+	// The modal needs some time to open
+	await waitForTime(100);
+
 	// Find and click the "search web" switch is on
 	const switchElement = await querySelectorAsync<HTMLButtonElement>(
 		'button[role="switch"]',
 	);
-	if (switchElement && switchElement.getAttribute("aria-checked") === "true") {
+
+	if (switchElement.getAttribute("aria-checked") === "true") {
 		switchElement.click();
 
 		// Wait for the switch to take effect
-		await waitForTime(300);
+		await waitForTime(500);
 	}
 }
 
